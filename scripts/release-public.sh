3a2f09b2d160a4b75d0b5f3734c8e0cc2e3e44f4
Matthew Slipper <me@matthewslipper.com>
2020-05-03 02:11:04 -0700

    Complete release script

--- scripts/release-public.sh
+++ scripts/release-public.sh
@@ -8,16 +8,16 @@ tag="$(git describe --tags --abbrev=0)"
 version="${tag:1}"
 
 create_tarball() {
-  mv -f "$build_dir/ddrpd-$0-amd64" "$build_dir/ddrpd"
-  mv -f "$build_dir/ddrpcli-$0-amd64" "$build_dir/ddrpcli"
-  tar -czvf "$build_dir/ddrp-$version-$0-amd64.tgz" -C "$build_dir" "ddrpd" "ddrpcli"
-  gpg2 --detach-sig --default-key D4B604F1 --output "$build_dir/ddrp-$version-$0-amd64.tgz.sig" "$build_dir/ddrp-$version-$0-amd64.tgz.sig"
+  mv -f "$build_dir/ddrpd-$1-amd64" "$build_dir/ddrpd"
+  mv -f "$build_dir/ddrpcli-$1-amd64" "$build_dir/ddrpcli"
+  tar -czvf "$build_dir/ddrp-$version-$1-amd64.tgz" -C "$build_dir" "ddrpd" "ddrpcli"
+  gpg2 --detach-sig --default-key D4B604F1 --output "$build_dir/ddrp-$version-$1-amd64.tgz.sig" "$build_dir/ddrp-$version-$1-amd64.tgz"
 }
 
 upload_binary() {
-  echo "Uploading $0 binary..."
-  gothub upload --user ddrp-org --repo ddrp --tag "$tag" --file "$build_dir/ddrp-$version-$0-amd64.tgz" --name "ddrp-$version-$0-amd64.tgz"
-  gothub upload --user ddrp-org --repo ddrp --tag "$tag" --file "$build_dir/ddrp-$version-$0-amd64.tgz.sig" --name "ddrp-$version-$0-amd64.tgz.sig"
+  echo "Uploading $1 binary..."
+  gothub upload --user ddrp-org --repo ddrp --tag "$tag" --file "$build_dir/ddrp-$version-$1-amd64.tgz" --name "ddrp-$version-$1-amd64.tgz"
+  gothub upload --user ddrp-org --repo ddrp --tag "$tag" --file "$build_dir/ddrp-$version-$1-amd64.tgz.sig" --name "ddrp-$version-$1-amd64.tgz.sig"
 }
 
 make clean
@@ -26,25 +26,21 @@ make package-deb version="$version"
 
 create_tarball "linux"
 create_tarball "darwin"
+gpg2 --detach-sig --default-key D4B604F1 --output "$build_dir/ddrp-$version-amd64.deb.sig" "$build_dir/ddrp-$version-amd64.deb"
 
 gothub release --user ddrp-org --repo ddrp --tag "$tag" --name "$tag" --description ""
-
 upload_binary "linux"
 upload_binary "darwin"
-
 echo "Uploading .deb..."
 gothub upload --user ddrp-org --repo ddrp --tag "$tag" --file "$build_dir/ddrp-$version-amd64.deb" --name "ddrp-$version-amd64.deb"
+gothub upload --user ddrp-org --repo ddrp --tag "$tag" --file "$build_dir/ddrp-$version-amd64.deb.sig" --name "ddrp-$version-amd64.deb.sig"
 
 # for seed node deployments
 
-#s3cmd put "$build_dir/ddrp-linux-amd64.tgz" s3://ddrp-releases/ddrp-linux-amd64.tgz
-#s3cmd setacl s3://ddrp-releases/ddrp-linux-amd64.tgz --acl-public
-#s3cmd put "$build_dir/ddrp-darwin-amd64.tgz" s3://ddrp-releases/ddrp-darwin-amd64.tgz
-#s3cmd setacl s3://ddrp-releases/ddrp-darwin-amd64.tgz --acl-public
-#s3cmd put "$build_dir/ddrp-linux-amd64.tgz.sig" s3://ddrp-releases/ddrp-linux-amd64.tgz.sig
-#s3cmd setacl s3://ddrp-releases/ddrp-linux-amd64.tgz.sig --acl-public
-#s3cmd put "$build_dir/ddrp-darwin-amd64.tgz.sig" s3://ddrp-releases/ddrp-darwin-amd64.tgz.sig
-#s3cmd setacl s3://ddrp-releases/ddrp-darwin-amd64.tgz.sig --acl-public
-#cd "$build_dir" && shasum -a 256 ddrp-linux-amd64.tgz > /tmp/ddrp-linux-amd64.tgz.sum.txt && cd "$DIR"
-#s3cmd put /tmp/ddrp-linux-amd64.tgz.sum.txt s3://ddrp-releases/ddrp-linux-amd64.tgz.sum.txt
-#s3cmd setacl s3://ddrp-releases/ddrp-linux-amd64.tgz.sum.txt --acl-public
+s3cmd put "$build_dir/ddrp-$version-linux-amd64.tgz" s3://ddrp-releases/ddrp-linux-amd64.tgz
+s3cmd setacl s3://ddrp-releases/ddrp-linux-amd64.tgz --acl-public
+s3cmd put "$build_dir/ddrp-$version-linux-amd64.tgz.sig" s3://ddrp-releases/ddrp-linux-amd64.tgz.sig
+s3cmd setacl s3://ddrp-releases/ddrp-linux-amd64.tgz.sig --acl-public
+cd "$build_dir" && shasum -a 256 "ddrp-$version-linux-amd64.tgz" > /tmp/ddrp-linux-amd64.tgz.sum.txt && cd "$DIR"
+s3cmd put /tmp/ddrp-linux-amd64.tgz.sum.txt s3://ddrp-releases/ddrp-linux-amd64.tgz.sum.txt
+s3cmd setacl s3://ddrp-releases/ddrp-linux-amd64.tgz.sum.txt --acl-public
